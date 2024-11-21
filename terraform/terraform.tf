a0265fb9d7ac5ceed98119723be31a17ed7b1f3c
Jan Ulrych <ulrych@marbes.cz>
2024-11-21 21:37:19 +0100

    cleanup

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -13,13 +13,13 @@ provider "opennebula" {
   password      = "${var.opennebula_password}"
 }
 
-# resource "opennebula_image" "os-image" {
-#     name = "DEMO - Ubuntu Minimal 24.04"
-#     datastore_id = 101
-#     persistent = false
-#     path = "https://marketplace.opennebula.io//appliance/44077b30-f431-013c-b66a-7875a4a4f528/download/0"
-#     permissions = "600"
-# }
+resource "opennebula_image" "os-image" {
+    name = "SP1 - Ubuntu Minimal 24.04"
+    datastore_id = 101
+    persistent = false
+    path = "https://marketplace.opennebula.io//appliance/44077b30-f431-013c-b66a-7875a4a4f528/download/0"
+    permissions = "600"
+}
 
 resource "opennebula_virtual_machine" "backend-node" {
   count = var.cluster_size
@@ -42,7 +42,7 @@ resource "opennebula_virtual_machine" "backend-node" {
   }
 
   disk {
-    image_id = 688 #opennebula_image.os-image.id
+    image_id = opennebula_image.os-image.id
     target   = "vda"
     size     = 6000 # 6GB
   }
@@ -88,7 +88,7 @@ resource "opennebula_virtual_machine" "frontend-node" {
   }
 
   disk {
-    image_id = 688 #opennebula_image.os-image.id
+    image_id = opennebula_image.os-image.id
     target   = "vda"
     size     = 6000 # 6GB
   }
@@ -132,12 +132,4 @@ resource "local_file" "hosts_cfg" {
       backend_nodes = opennebula_virtual_machine.backend-node.*.ip
     })
   filename = "../dynamic_inventories/cluster"
-}
-
-resource "local_file" "nginx_cfg" {
-  content = templatefile("config.tmpl",
-    {
-      backend_nodes = opennebula_virtual_machine.backend-node.*.ip
-    })
-  filename = "../dynamic_inventories/backend-proxy.conf"
 }
